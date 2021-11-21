3cc92b82e7ef65e9482df37651a3c507a42e0415
GiovannyRosas <giovannyrm.8@gmail.com>
2021-11-21 12:48:20 -0500

    first commit

---
+++ src/database.js
@@ -0,0 +1,11 @@
+import { connect } from "mongoose";
+import { MONGODB_URI } from './config'
+
+(async () => {
+  try {
+    const db = await connect(MONGODB_URI);
+    console.log("Db connected to", db.connection.name);
+  } catch (err) {
+    console.log(err);
+  }
+})();
